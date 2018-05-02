224b5708a1e973ca1a15486877320a773d452262
Kartik Arora <kartikar@uw.edu>
2018-05-02 13:43:42 -0700

    complete stage 13

--- zombie.sol
+++ zombie.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.19;
 
 contract ZombieFactory {
 
+    // creating an event to tell zombie created
+    event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
@@ -11,10 +13,13 @@ contract ZombieFactory {
         uint dna;
     }
 
-    Zombie[] public zombies;
+    Zombie[] public zombies; // Array of type zombies
 
     function _createZombie(string _name, uint _dna) private {
-        zombies.push(Zombie(_name, _dna));
+        // array.push()-1 returns value last added to array
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        //Firing the event
+        NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string _str) private view returns (uint) {
