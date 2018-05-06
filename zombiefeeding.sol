10993e7c091ea36a7e128537fd7d9741964098f6
Kartik Arora <kartikar@uw.edu>
2018-05-05 23:17:43 -0700

    craete new zombie

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -1,10 +1,14 @@
 import "./zombiefactory.sol";
 
 contract zombieFeeding is zombieFactory {
+
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
     require(msg.sender == zombieToOwner[_zombieId]); // requires feeder to be owner
     //storage stores to blockchain
-    //memory stores to ram 
+    //memory stores to ram
     Zombie storage myZombie = zombies[_zombieId]; // adding zombie to blockchain storage
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna)/2;
+    _createZombie("NoName", newDna);
   }
 }
