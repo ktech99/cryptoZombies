c142a0ecb755b46e783d9c56b6b05161b04167da
Kartik Arora <kartikar@uw.edu>
2018-05-06 16:39:47 -0700

    create interface of crypto kitty getKitty method

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -1,5 +1,21 @@
 import "./zombiefactory.sol";
 
+//creates an interface of cryptokitty method
+contract KittyInterface {
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
+
 contract zombieFeeding is zombieFactory {
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
