6ddf1d1440c323ebcd6770c26ca2683ebf3eb750
Kartik Arora <kartikar@uw.edu>
2018-05-08 17:51:43 -0700

    add level and ready time to game

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -13,6 +13,8 @@ contract ZombieFactory is Ownable{
   struct Zombie {
     string name;
     uint dna;
+    uint32 level;
+    uint32 readyTime; //have similar types together to reduce gas
   }
 
   Zombie[] public zombies; // Array of type zombies
