aa172b42359775a9da678d54b0f3be8db0ae831d
Kartik Arora <kartikar@uw.edu>
2018-05-12 13:20:14 -0700

    add level based features

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -6,4 +6,16 @@ contract Zombiehelper is Zombiefeeding{
     require(zombies[_zombieID].level >= level);
     _;
   }
+
+  // be allowed to change name after level 2
+  function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId){
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].name = _newName;
+  }
+
+  // be allowed to change dna after level 20
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId){
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].dna = _newDna;
+  }
 }
